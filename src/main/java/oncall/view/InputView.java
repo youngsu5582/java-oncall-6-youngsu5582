7d55f73f3742cd200bb4630869783a3c77896193
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-16 14:32:51 +0900

    feat(View) : inputView 에 DayInfo 받는 기능 구현 추가

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,6 +1,12 @@
 package oncall.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import oncall.domain.DayInfo;
+import oncall.domain.DayOfWeek;
+import oncall.util.Parser;
+import oncall.util.Validator;
+
+import java.util.List;
 
 public class InputView {
     private static InputView INSTANCE = null;
@@ -20,4 +26,14 @@ public class InputView {
 
         return readInput();
     }
+
+    public DayInfo inputDayInfo() {
+        String dayInfo = readInput();
+        Validator.validateInfoWithPattern(dayInfo);
+        List<String> dayInfoList = Parser.parseInfoWithSeparator(dayInfo, ",");
+        Integer month = Parser.parseInfoToNumber(dayInfoList.get(0));
+        DayOfWeek dayOfWeek = DayOfWeek.getDayOfWeekWithInfo(dayInfoList.get(1));
+        return new DayInfo(month, dayOfWeek);
+    }
+
 }
--- src/main/java/oncall/view/message/InputViewMessage.java
+++ src/main/java/oncall/view/message/InputViewMessage.java
@@ -1,7 +1,9 @@
 package oncall.view.message;
 
 public enum InputViewMessage {
-    ;
+    EMERGENCY("비상 근무를 배정할 월과 시작 요일을 입력하세요> "),
+    WEEKDAY("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> "),
+    WEEKEND("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
     private final String message;
 
     InputViewMessage(String message) {
