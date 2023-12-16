2b054ce812780a1afea9c697fa86c92b1500c17e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-16 16:35:48 +0900

    feat(util) : Validator 에 정규식 변경

--- src/main/java/oncall/util/Validator.java
+++ src/main/java/oncall/util/Validator.java
@@ -5,10 +5,10 @@ import oncall.exception.ExceptionMessage;
 import java.util.regex.Pattern;
 
 public class Validator {
-    private static Pattern pattern = Pattern.compile("^([a-zA-Z가-힣]+,)*[a-zA-Z가-힣]+$");
+    private static Pattern pattern = Pattern.compile("^([a-zA-Z가-힣\\d]+,)*[a-zA-Z가-힣\\d]+$");
 
     public static void validateInfoWithPattern(String info) {
-        if (pattern.matcher(info).matches())
+        if (!pattern.matcher(info).matches())
             throw new IllegalArgumentException(ExceptionMessage.INVALID_FORMAT.getMessage());
     }
 }
