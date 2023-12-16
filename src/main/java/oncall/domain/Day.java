838082cd426d12bd80cd152c1adb336ea24c0016
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-16 16:36:35 +0900

    feat(Domain) : 월 , 일 저장하는 Domain 추가

---
+++ src/main/java/oncall/domain/Day.java
@@ -0,0 +1,11 @@
+package oncall.domain;
+
+public class Day {
+    public Integer month;
+    public Integer date;
+
+    public Day(Integer month, Integer date) {
+        this.month = month;
+        this.date = date;
+    }
+}
