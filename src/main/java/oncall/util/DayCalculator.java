7935ab73cc99f6e1fabc630af4957660b441979c
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-16 16:38:00 +0900

    feat(Util) : 현재 달 과 현재 달중 가장 큰 날을 돌려주는 기능 추가

--- src/main/java/oncall/util/DayCalculator.java
+++ src/main/java/oncall/util/DayCalculator.java
@@ -8,6 +8,14 @@ import java.util.List;
 public class DayCalculator {
     private static List<Integer> lastDayList = List.of(0, 31, 28, 31, 30, 31, 30, 31, 31, 31, 30, 31, 30, 31);
 
+    public Integer getMaxDayInCurrentMonth() {
+        return lastDayList.get(this.dayInfo.getCurrentMonth());
+    }
+
+    public Integer getCurrentMonth() {
+        return this.dayInfo.getCurrentMonth();
+    }
+
     private final DayInfo dayInfo;
 
     public DayCalculator(DayInfo dayInfo) {
