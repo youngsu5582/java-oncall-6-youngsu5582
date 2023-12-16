f3894a0676b7385c28ca82acda7cb398345f0c0f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-16 14:32:09 +0900

    feat(Util) : 상수 담당하는 Constant 추가

---
+++ src/main/java/oncall/util/Constant.java
@@ -0,0 +1,8 @@
+package oncall.util;
+
+public class Constant {
+    public static final Integer WORKER_MIN_SIZE = 5;
+    public static final Integer WORKER_MAX_SIZE = 35;
+    public static final Integer NICKNAME_MAX_SIZE = 5;
+
+}
