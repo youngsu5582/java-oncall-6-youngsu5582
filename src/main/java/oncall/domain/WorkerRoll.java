2a2c2b437e4e5b2117c3336f72c9df2754917c0d
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-16 16:44:50 +0900

    feat(Domain) : 평일 , 주말 명단 관리하는 Domain 추가

---
+++ src/main/java/oncall/domain/WorkerRoll.java
@@ -0,0 +1,42 @@
+package oncall.domain;
+
+import oncall.exception.ExceptionMessage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WorkerRoll {
+    private List<String> weekdayTurnRoll;
+    private List<String> weekendTurnRoll;
+
+    public WorkerRoll(WorkersInfo weekdayInfo, WorkersInfo weekendInfo) {
+        validateWorkerRoll(weekdayInfo, weekendInfo);
+        this.weekendTurnRoll = weekendInfo.getWorkerNicknames();
+        this.weekdayTurnRoll = weekdayInfo.getWorkerNicknames();
+    }
+
+    public Integer getWorkerSize() {
+        return weekdayTurnRoll.size();
+    }
+
+    public String getWeekdayWorker(Integer count) {
+        return this.weekdayTurnRoll.get(count);
+    }
+
+    public String getWeekendWorker(Integer count) {
+        return this.weekendTurnRoll.get(count);
+    }
+
+    private void validateWorkerRoll(WorkersInfo weekdayInfo, WorkersInfo weekendInfo) {
+        validateEqual(weekdayInfo.getWorkerNicknames(), weekendInfo.getWorkerNicknames());
+    }
+
+    private void validateEqual(List<String> weekdayWorkerNicknames, List<String> weekendWorkerNicknames) {
+        List<String> sortedList1 = weekdayWorkerNicknames.stream().sorted().collect(Collectors.toList());
+        List<String> sortedList2 = weekendWorkerNicknames.stream().sorted().collect(Collectors.toList());
+        if (sortedList1.equals(sortedList2)) {
+            return;
+        }
+        throw new IllegalStateException(ExceptionMessage.INVALID_FORMAT.getMessage());
+    }
+}
